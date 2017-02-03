fe6f786a208c2d5761ba0b9f5b9265552a06e166
mndowne <mndowne@gmail.com>
2017-02-02 20:29:15 -0600

    work in progress

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+    res.render(__dirname + "/index.html");
+});
+
+app.listen(app.get('port'), function() {
+    console.log('server is up on the process.env.PORT or port 3000');
+});
